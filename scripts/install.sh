a0c78e716c8955e55478492f4749b346c8255ed8
Nguyen Thanh An <73547738+thanhan92-f1@users.noreply.github.com>
2025-05-04 02:32:13 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -102,7 +102,7 @@ php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 # setup website
 export COMPOSER_ALLOW_SUPERUSER=1
-export V_REPO="https://github.com/vitodeploy/vito.git"
+export V_REPO="https://github.com/thanhan92-f1/hitechcloudpanel.git"
 export V_VHOST_CONFIG="
 server {
     listen 80;
